a9ede784ac3392ccd89a4237a1d042ee1fae0016
alosdev <hasanhosgel@googlemail.com>
2013-04-03 18:00:08 +0200

    create simple list view

--- src/de/alosdev/listviewdemo/MainActivity.java
+++ src/de/alosdev/listviewdemo/MainActivity.java
@@ -2,13 +2,31 @@ package de.alosdev.listviewdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemClickListener {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ListView list = (ListView) findViewById(R.id.list);
+    String[] array = getResources().getStringArray(R.array.list_view_examples);
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+      android.R.id.text1, array);
+    list.setAdapter(adapter);
+    list.setOnItemClickListener(this);
+  }
+
+  @Override
+  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    Toast.makeText(this, "you selected position: " + (position + 1), Toast.LENGTH_SHORT).show();
   }
 
 }
