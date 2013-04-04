bbb6308993034be7bd20fb78a65548f2a666edb4
alosdev <hasanhosgel@googlemail.com>
2013-04-04 17:46:48 +0200

    adds List activity

--- src/de/alosdev/listviewdemo/ListActivity.java
+++ src/de/alosdev/listviewdemo/ListActivity.java
@@ -2,46 +2,28 @@ package de.alosdev.listviewdemo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity implements OnItemClickListener {
+public class ListActivity extends android.app.ListActivity {
   private ArrayAdapter<String> adapter;
 
-  @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
 
-    ListView list = (ListView) findViewById(R.id.list);
     String[] array = getResources().getStringArray(R.array.list_view_examples);
     adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
       android.R.id.text1, new ArrayList<String>(Arrays.asList(array)));
-    list.setAdapter(adapter);
-    list.setOnItemClickListener(this);
+    setListAdapter(adapter);
   }
 
   @Override
-  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+  protected void onListItemClick(ListView l, View v, int position, long id) {
     switch (position) {
-      case 1: {
-        startActivity(new Intent(this, CustomAdapterActivity.class));
-        break;
-      }
-
-      case 2: {
-        startActivity(new Intent(this, ListActivity.class));
-        break;
-      }
-
       case 3: {
         adapter.add("added item " + position);
         break;
@@ -53,4 +35,5 @@ public class MainActivity extends Activity implements OnItemClickListener {
       }
     }
   }
+
 }
